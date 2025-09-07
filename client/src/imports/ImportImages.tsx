a5b4d998d5c933e9c218f9d0740d1088ddbbac47
JUNGHO-GIT <junghomun00@gmail.com>
2025-09-07 19:44:47 +0900

    2025-09-07 19:44:47

--- client/src/imports/ImportImages.tsx
+++ client/src/imports/ImportImages.tsx
@@ -1,8 +1,38 @@
 // importImages.jsx
 
 // main
+export { default as logo } from "@assets/images/main/logo.png";
+export { default as main } from "@assets/images/main/main.png";
+export { default as main2 } from "@assets/images/main/main2.png";
+export { default as profile } from "@assets/images/main/profile.png";
+export { default as tab } from "@assets/images/main/tab.png";
+
 // projects 1
+export { default as project1_1 } from "@assets/images/projects/project1_1.png";
+export { default as project1_2 } from "@assets/images/projects/project1_2.png";
+
 // projects 2
+export { default as project2_1 } from "@assets/images/projects/project2_1.png";
+export { default as project2_2 } from "@assets/images/projects/project2_2.png";
+export { default as project2_3 } from "@assets/images/projects/project2_3.png";
+export { default as project2_4 } from "@assets/images/projects/project2_4.png";
+export { default as project2_5 } from "@assets/images/projects/project2_5.png";
+
 // projects 3
+export { default as project3_1 } from "@assets/images/projects/project3_1.png";
+export { default as project3_2 } from "@assets/images/projects/project3_2.png";
+export { default as project3_3 } from "@assets/images/projects/project3_3.png";
+export { default as project3_4 } from "@assets/images/projects/project3_4.png";
+export { default as project3_5 } from "@assets/images/projects/project3_5.png";
+
 // projects 4
+export { default as project4_1 } from "@assets/images/projects/project4_1.png";
+export { default as project4_2 } from "@assets/images/projects/project4_2.png";
+export { default as project4_3 } from "@assets/images/projects/project4_3.png";
+export { default as project4_4 } from "@assets/images/projects/project4_4.png";
+
 // projects 5
+export { default as project5_1 } from "@assets/images/projects/project5_1.png";
+export { default as project5_2 } from "@assets/images/projects/project5_2.png";
+export { default as project5_3 } from "@assets/images/projects/project5_3.png";
+export { default as project5_4 } from "@assets/images/projects/project5_4.png";
