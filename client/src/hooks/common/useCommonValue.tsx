a3f28a718bf8ccaf2baaf0273240e2b1eab9f33d
JUNGHO-GIT <junghomun00@gmail.com>
2025-11-23 01:39:52 +0900

    2025-11-23  1:39:52

--- client/src/hooks/common/useCommonValue.tsx
+++ client/src/hooks/common/useCommonValue.tsx
@@ -5,29 +5,45 @@ import { useLocation, useNavigate } from "@exportReacts";
 // -------------------------------------------------------------------------------------------------
 export const useCommonValue = () => {
 
-  // -----------------------------------------------------------------------------------------------
-  const navigate = useNavigate();
-  const location = useLocation();
-  const PATH = location.pathname;
-  const pathParts = PATH.split("/");
+  // location
+  const navigate: any = useNavigate();
+  const location: any = useLocation();
+  const location_id: string = location?.state?._id;
+  const location_category: string = location?.state?.category;
+  const PATH: string = location?.pathname;
+  const firstStr: string = PATH?.split("/")[1] || "";
+  const secondStr: string = PATH?.split("/")[2] || "";
+  const thirdStr: string = PATH?.split("/")[3] || "";
 
-  // -----------------------------------------------------------------------------------------------
-  return {
-    navigate,
-    location,
-    location_id: location?.state?.id,
-    location_category: location?.state?.category,
-    PATH,
-    firstStr: pathParts[1] || "",
-    secondStr: pathParts[2] || "",
-    thirdStr: pathParts[3] || "",
-    TITLE: import.meta.env.VITE_APP_TITLE || "",
-    URL: import.meta.env.VITE_APP_SERVER_URL || "",
-    SUBFIX: import.meta.env[`VITE_APP_${pathParts[1]?.toUpperCase()}_URL`] || "",
-    GCLOUD_URL: import.meta.env.VITE_APP_GCLOUD_URL || "",
-    PROJECTS_URL: import.meta.env.VITE_APP_PROJECTS_URL || "",
-    ABOUT_URL: import.meta.env.VITE_APP_ABOUT_URL || "",
-    SKILLS_URL: import.meta.env.VITE_APP_SKILLS_URL || "",
-    PORTFOLIOS_URL: import.meta.env.VITE_APP_PORTFOLIOS_URL || "",
-  };
+  // env
+  const TITLE: string = import.meta.env.VITE_APP_TITLE || "";
+  const URL: string = import.meta.env.VITE_APP_SERVER_URL || "";
+  const SUBFIX_KEY = `VITE_APP_${firstStr.toUpperCase()}_URL`;
+  const SUBFIX: string = (import.meta.env as unknown as Record<string, string>)[SUBFIX_KEY] || "";
+  const GCLOUD_URL: string = import.meta.env.VITE_APP_GCLOUD_URL || "";
+	const PROJECTS_URL: string = import.meta.env.VITE_APP_PROJECTS_URL || "";
+	const ABOUT_URL: string = import.meta.env.VITE_APP_ABOUT_URL || "";
+	const SKILLS_URL: string = import.meta.env.VITE_APP_SKILLS_URL || "";
+	const PORTFOLIOS_URL: string = import.meta.env.VITE_APP_PORTFOLIOS_URL || "";
+
+	// -----------------------------------------------------------------------------------------------
+	return {
+		navigate,
+		location,
+		location_id,
+		location_category,
+		PATH,
+		firstStr,
+		secondStr,
+		thirdStr,
+		TITLE,
+		URL,
+		SUBFIX_KEY,
+		SUBFIX,
+		GCLOUD_URL,
+		PROJECTS_URL,
+		ABOUT_URL,
+		SKILLS_URL,
+		PORTFOLIOS_URL
+	};
 };
